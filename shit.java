2404ae5802fc45883a0ed5f543919d775f4ea771
Tyler Foster <jfoster39@gatech.edu>
2013-06-07 15:33:12 -0400

    MORE HELLOWORD

--- shit.java
+++ shit.java
@@ -1,5 +1,6 @@
 public class shit {
     public static void main(String[] args) {
         System.out.println("What up thug! I'm hip with the new generation!");
+        System.out.println("More stuff!");
     }
 }
