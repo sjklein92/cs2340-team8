f90a07e3bc22003cd18c97403a98e34099622d47
Tyler Foster <jfoster39@gatech.edu>
2013-06-10 13:00:25 -0400

    changed filename

---
+++ stuff.java
@@ -0,0 +1,10 @@
+public class stuff {
+    public static void main(String[] args) {
+        System.out.println("What up thug! I'm hip with the new generation!");
+        System.out.println("More stuff!");
+        System.out.println("AGAIN");
+        
+        Junk badVariableName = new Junk(45);
+        System.out.println(badVariableName.getNumber());
+    }
+}
