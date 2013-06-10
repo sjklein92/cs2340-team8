15734a71cb5f0e5c65875a4bfd00ed4b37e92d7e
Brad Ware <bware8@gatech.edu>
2013-06-10 15:57:26 -0400

    javadoc'd player

--- Player.java
+++ Player.java
@@ -7,18 +7,35 @@ public class Player {
 	public Player(String name){
 		this.name = name;
 	}
+	/**
+	 * 
+	 * @return Number of fleets a player has
+	 */
 	
 	public int getFleets(){
 		return numFleets;
 	}
 	
+	/**
+	 * Called when a player is attacked, removes a player's fleet
+	 */
 	public void removeFleet(){
 		numFleets--;
 	}
 	
+	/**
+	 * Sets the number of fleets a Player has based on how many player's 
+	 * Are playing risk
+	 * @param newNumFleets
+	 */
 	public void setNumFleets(int newNumFleets){
 		numFleets = newNumFleets;
 	}
+	
+	/**
+	 * 
+	 * @return Name of the player
+	 */
 	
 	public String getName(){
 		return name;
