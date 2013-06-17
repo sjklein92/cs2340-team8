efd09f6d469d1301e64b40c2e58f9355bafedec7
Brad Ware <bware8@gatech.edu>
2013-06-17 10:11:30 -0400

    Java Doc the Planet class

--- webapp/src/main/java/edu/gatech/cs2340/risk/model/Planet.java
+++ webapp/src/main/java/edu/gatech/cs2340/risk/model/Planet.java
@@ -5,6 +5,7 @@ package edu.gatech.cs2340.risk.model;
 
 // Added stuff
 /**
+ * This is the class for a Planet, which is a terrritory in our version of Risk
  * This class contains pointers to previous and next planets in its star system
  *
  */
@@ -13,22 +14,33 @@ public class Planet
 
     Planet prevPlanet, nextPlanet = null;
 
+    /**
+    *This sets the prevPlanet for the current Planet
+    *@param prevPlanet: the prev planet in the solar system
+    */
+
     public void setPrevPlanet(Planet prevPlanet) {
         this.prevPlanet = prevPlanet;
     }
     
     /**
-     * 
+     * This set's the current Planet's next Planet
      * @param nextPlanet: the next planet in the star system 
      */
     public void setNextPlanet(Planet nextPlanet) {
         this.nextPlanet = nextPlanet;
     }
-    
-   public Planet getPrevPlanet() {
+    /**
+    * This get's the current Planet's prevPlanet in the solar system
+    * @return prevPlanet: the prev planet in the solar system
+    */
+    public Planet getPrevPlanet() {
        return prevPlanet;
-   }
-    
+    }
+    /**
+    * This get's the current Planet's nextPlanet in the solar system
+    * @return nextPlanet: the next planet in the solar system
+    */
     public Planet getNextPlanet() {
         return nextPlanet;
     }
