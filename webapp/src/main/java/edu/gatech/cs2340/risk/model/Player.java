5f177815bd9b108d5af4f43944467dbbc63c0718
Brad Ware <bware8@gatech.edu>
2013-06-17 10:29:22 -0400

    Java doc version 3

--- webapp/src/main/java/edu/gatech/cs2340/risk/model/Player.java
+++ webapp/src/main/java/edu/gatech/cs2340/risk/model/Player.java
@@ -1,32 +1,57 @@
 package edu.gatech.cs2340.risk.model;
 
+/**
+* This class represents a player in Star Galaxy Risk
+*/
 public class Player {
 	private String name;
 	//private Planet planet;
 	private int numFleets;
 	private String color;
+
+	/**
+	* Sets a Player's constructor to take in a Name, and a Color
+	* @param Name, Color: String's that set the Player's name and Color
+	*/
 	
 	public Player(String name, String color){
 		this.name = name;
 		this.color = color;
 	}
+
+	/**
+	* Getter method for the number of fleets associated with the player
+	* @return the number of fleets this player has
+	*/
 	
 	public int getFleets(){
 		return numFleets;
 	}
 	
+	/**
+	* Decrements the number of fleets a player has by 1
+	*/
 	public void removeFleet(){
 		numFleets--;
 	}
-	
+	/**
+	* This method set's the number of fleets a player has to the Integer it takes in as a parameter
+	* @param newNumFleets: the new number of fleets the player will have
+	*/
 	public void setNumFleets(int newNumFleets){
 		numFleets = newNumFleets;
 	}
-	
+	/**
+	* This method return the Player's current name
+	* @return String name of the player
+	*/
 	public String getName(){
 		return name;
 	}
-	
+	/**
+	* This method return the Player's current color
+	* @return String color of the player
+	*/
 	public String getColor(){
 		return color;
 	}
