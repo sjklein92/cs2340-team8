bcef746160091fe1873b23519d96fda6cfe28670
Steven Klein <sjklein92@gmail.com>
2013-07-16 22:00:27 -0400

    Delete attackScript.js
    Unnecessary addition, logic handled by drop-down script in map.jsp

--- webapp/src/main/webapp/attackScript.js
+++
@@ -1,7 +0,0 @@
-function attackPrompt(planetID) {
-      var promptMessage = "Which planet's fleets will you be using to attack planet {0}?";
-      String.Format(promptMessage, planetID);
-      var attackingPlanetID = prompt(promptMessage);
-      
-      
-    }
