29a85e8a69e59fe7a6fd51847e8cd7698e69c232
Tyler Foster <jfoster39@gatech.edu>
2013-07-23 23:05:01 -0400

    put in default message again

--- webapp/src/main/java/edu/gatech/cs2340/risk/model/GameLogic.java
+++ webapp/src/main/java/edu/gatech/cs2340/risk/model/GameLogic.java
@@ -22,7 +22,7 @@ public class GameLogic
         numPlayers = players.size();
         allSystems = new ArrayList<StarSystem>();
         turnCount = 0;
-        log = "";
+        log =  "No attacks have been made.";
         
         for(Player player: players){
             allSystems.add(new StarSystem(player));
