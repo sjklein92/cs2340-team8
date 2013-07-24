f501e376be6e6a9ee75d48c1afb9f2fdeb252e09
Tyler Foster <jfoster39@gatech.edu>
2013-07-23 22:49:07 -0400

    looks better with no message first time

--- webapp/src/main/java/edu/gatech/cs2340/risk/model/GameLogic.java
+++ webapp/src/main/java/edu/gatech/cs2340/risk/model/GameLogic.java
@@ -22,7 +22,7 @@ public class GameLogic
         numPlayers = players.size();
         allSystems = new ArrayList<StarSystem>();
         turnCount = 0;
-        log = "No attacks have been made.";
+        log = "";
         
         for(Player player: players){
             allSystems.add(new StarSystem(player));
